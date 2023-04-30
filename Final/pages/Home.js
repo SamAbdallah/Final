7adc0d4b273ff6434a149656fe6905f5ff7d7aad
SamAbdallah <saa081@student.bau.edu.lb>
2023-04-30 15:53:08 +0300

    check if database exsits or not

--- Final/pages/Home.js
+++ Final/pages/Home.js
@@ -4,12 +4,36 @@ import { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Button } from '@rneui/themed';
+import * as SQLite from 'expo-sqlite';
+import { openDatabase } from 'expo-sqlite';
 
+ 
 function Home({ Name, Email }) {
   // const l=["Bruschetta.png","Greek salad.png","Grilled fish.png","Lemon dessert.png","Pasta.png"]
   // console.log(l[Math.floor(Math.random() * l.length)])
   const [image, setImage] = useState('');
   const [data, setData] = useState([]);
+
+  const dbName = 'myDatabase.db';
+
+  const db = openDatabase(dbName);
+  
+  db.transaction((tx) => {
+    tx.executeSql(
+      `SELECT name FROM sqlite_master WHERE type='table' AND name='myTable';`,
+      [],
+      (_, result) => {
+        if (result.rows.length > 0) {
+          console.log('The database exists and contains a table called "myTable"');
+        } else {
+          console.log('The database does not exist or does not contain a table called "myTable"');
+        }
+      }
+    );
+  });
+
+
+
 
   useEffect(() => {
     fetch('https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json')
@@ -18,8 +42,7 @@ function Home({ Name, Email }) {
         setData(data.menu);
       });
   }, []);
-
-  useEffect(() => {
+  useEffect(() => { 
     const retrieveData = async () => {
       try {
         const IMAGE = await AsyncStorage.getItem('image');
@@ -61,9 +84,9 @@ function Home({ Name, Email }) {
           <Text style={styles.paragraph}>
             We are a family owned mediterranean restaurant,focused on traditional recipes served with a modern twist
           </Text>
-          <Image style={styles.image2} source={require('../assets/images/Grilled fish.png')} resizeMode="contain" />
+          <Image style={styles.image3} source={require('../assets/images/Pasta.png')} resizeMode="contain" />
         </View>
-
+ 
         <Icon name="search" size={30} color="black" />
       </View>
       <Text style={styles.Order}>Order For Delivery!</Text>
@@ -132,6 +155,13 @@ const styles = StyleSheet.create({
     flex: 0.4,
     borderRadius:10,
     height:130 ,
+    marginTop:-20,
+    marginLeft:30
+  },
+  image3: {
+    flex: 0.4,
+    borderRadius:10,
+    height:90 ,
     marginTop:-20,
     marginLeft:30
   },
@@ -211,99 +241,3 @@ const styles = StyleSheet.create({
 })
 
 export default Home;
-// import React from 'react';
-// import { Text, StyleSheet, View, Image, FlatList } from 'react-native';
-// import { useEffect, useState } from 'react';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import Icon from 'react-native-vector-icons/FontAwesome';
-// import { Button } from '@rneui/themed';
-
-// function Home({ Name, Email }) {
-//   const [image, setImage] = useState('');
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     fetch('https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json')
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setData(data.menu);
-//       });
-//   }, []);
-
-//   useEffect(() => {
-//     const retrieveData = async () => {
-//       try {
-//         const IMAGE = await AsyncStorage.getItem('image');
-//         if (IMAGE !== null) {
-//           setImage(IMAGE);
-//         }
-//       } catch (e) {
-//         console.log(e);
-//       }
-//     };
-//     retrieveData();
-//   }, []);
-
-  
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.header}>
-//         <Image style={styles.image} source={require('../assets/images/Logo.png')} resizeMode="contain" />
-//         {image ? (
-//           <Image
-//             source={{ uri: image }}
-//             style={{ height: 100, marginLeft: 20, width: 50, borderRadius: 10, flex: 0.4 }}
-//           />
-//         ) : (
-//           <View style={styles.dummy}>
-//             <Text>
-//               {Name[0]}
-//               {Name[1]}
-//             </Text>
-//           </View>
-//         )}
-//       </View>
-
-//       <View style={styles.intro}>
-//         <Text style={styles.title}>Little Lemon</Text>
-//         <Text style={styles.location}>Chicago</Text>
-
-//         <View style={styles.data}>
-//           <Text style={styles.paragraph}>
-//             We are a family owned mediterranean restaurant,focused on traditional recipes served with a modern twist
-//           </Text>
-//           <Image style={styles.image2} source={require('../assets/images/Grilledfish.png')} resizeMode="contain" />
-//         </View>
-
-//         <Icon name="search" size={30} color="black" />
-//       </View>
-//       <Text style={styles.Order}>Order For Delivery!</Text>
-
-//       <View style={styles.categroy}>
-//         <Button title="Starters" buttonStyle={styles.buttons} titleStyle={{ color: 'black' }} />
-//         <Button title="Main" buttonStyle={styles.buttons} titleStyle={{ color: 'black' }} />
-//         <Button title="Desserts" buttonStyle={styles.buttons} titleStyle={{ color: 'black' }} />
-//         <Button title="Drinks" buttonStyle={styles.buttons} titleStyle={{ color: 'black' }} />
-//       </View>
-
-//       <View style={{ borderBottomWidth: 1, borderBottomColor: 'lightgrey', margin: 10 }} />
-
-//       <FlatList
-//   style={styles.list}    
-//   data={data}
-//   renderItem={({ item }) => (
-//     <View>
-//       <Text>{item.name}</Text>
-//       <Text>{item.description}</Text>
-//     </View>
-//   )}
-//   keyExtractor={(item, index) => index.toString()}
-//   contentContainerStyle={{ flexGrow: 1 }}
-
-// />
-
-
-// </View>
-
-//   );
-// }
